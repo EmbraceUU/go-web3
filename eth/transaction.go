22cd1834abdd6b3162336d3200acef6c235abdd4
EmbraceUU <1014435409@qq.com>
2022-03-31 19:07:34 +0800

    删除SendRawTransaction中的nonce

--- eth/transaction.go
+++ eth/transaction.go
@@ -57,14 +57,14 @@ func (e *Eth) SendRawTransaction(
 	gasPrice *big.Int,
 	data []byte,
 ) (common.Hash, error) {
-	nonce, err := e.GetNonce(e.address, nil)
+	//nonce, err := e.GetNonce(e.address, nil)
 	var hash common.Hash
-	if err != nil {
-		return hash, err
-	}
+	//if err != nil {
+	//	return hash, err
+	//}
 	// fmt.Printf("nonce %v\n", nonce)
 
-	tx := eTypes.NewTransaction(nonce, to, amount, gasLimit, gasPrice, data)
+	tx := eTypes.NewTransaction(0, to, amount, gasLimit, gasPrice, data)
 
 	// fmt.Println(tx)
 	signedTx, err := eTypes.SignTx(tx, eTypes.NewEIP155Signer(e.chainId), e.privateKey)
